9f7e5958623f781f7c7150267265e86b2e194dff
aalhendi <abdulrazaq.alhendi@gmail.com>
2021-12-08 11:46:13 +0300

    Added sidenav core

--- app/lib/view/home.dart
+++ app/lib/view/home.dart
@@ -1,22 +1,65 @@
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget{
+class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
-@override
-Widget build(BuildContext context) {
-  return Scaffold(
-    appBar: AppBar(
-      title: const Text("Home"),
-      actions: [
-        IconButton(
-            onPressed: () => {
-                  showSearch(context: context, delegate: CustomSearchDelegate())
-                },
-            icon: const Icon(Icons.search))
-      ],
-    ),
-  );
-}
+  @override
+  Widget build(BuildContext context) {
+    showAlertDialog(BuildContext context) {
+      /* Button Setup */
+      Widget cancelButton = TextButton(
+        child: const Text("Cancel"),
+        onPressed: () {
+          Navigator.of(context).pop();
+        },
+      );
+      Widget continueButton = TextButton(
+        child: const Text("Continue"),
+        onPressed: () {},
+      );
+
+      /* Alert Dialog Setup */
+      AlertDialog alert = AlertDialog(
+        title: const Text("Alert"),
+        content: const Text("Settings Pressed"),
+        actions: [
+          cancelButton,
+          continueButton,
+        ],
+      );
+
+      /* Show Dialog */
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return alert;
+        },
+      );
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Home"),
+        backgroundColor: Colors.black87,
+        leading: Image.asset("images/logo.png"),
+        actions: [
+          IconButton(
+              onPressed: () => {
+                    showSearch(
+                        context: context, delegate: CustomSearchDelegate())
+                  },
+              highlightColor: Colors.yellow,
+              splashColor: Colors.blue,
+              icon: const Icon(Icons.search)),
+          IconButton(
+              onPressed: () {
+                showAlertDialog(context);
+              },
+              icon: const Icon(Icons.settings))
+        ],
+      ),
+      drawer: const Sidenav(),
+    );
+  }
 }
 
 class CustomSearchDelegate extends SearchDelegate {
@@ -28,8 +71,8 @@ class CustomSearchDelegate extends SearchDelegate {
     return [
       IconButton(
           onPressed: () {
-                query = '';
-              },
+            query = '';
+          },
           icon: const Icon(Icons.clear))
     ];
   }
@@ -69,7 +112,7 @@ class CustomSearchDelegate extends SearchDelegate {
         matchQuery.add(query);
       }
     }
-        return ListView.builder(
+    return ListView.builder(
         itemCount: matchQuery.length,
         itemBuilder: (context, index) {
           var result = matchQuery[index];
@@ -77,5 +120,105 @@ class CustomSearchDelegate extends SearchDelegate {
             title: Text(result),
           );
         });
+  }
+}
+
+class Sidenav extends StatelessWidget {
+  const Sidenav({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            // TODO: Font selection?
+            child: Text(
+              "Manager",
+              style: TextStyle(
+                  fontSize: 21, color: Theme.of(context).primaryColor),
+            ),
+          ),
+          Divider(
+            color: Colors.grey.shade400,
+          ),
+          ListTile(
+            title: const Text("Home"),
+            leading: IconButton(
+                onPressed: () {
+                  print("Pressed home");
+                },
+                icon: const Icon(
+                  Icons.home,
+                  color: Colors.black,
+                )),
+          ),
+          Divider(
+            color: Colors.grey.shade400,
+          ),
+          ListTile(
+            title: const Text("To Do"),
+            leading: IconButton(
+                onPressed: () {
+                  print("Pressed Todo");
+                },
+                icon: const Icon(
+                  Icons.list_alt,
+                  color: Colors.black,
+                )),
+            // TODO: count incomplete todos
+            trailing: const Text(
+              "2",
+              style: TextStyle(fontWeight: FontWeight.w500),
+            ),
+          ),
+          ListTile(
+            title: const Text("Calendar"),
+            leading: IconButton(
+                onPressed: () {
+                  print("Pressed Calendar");
+                },
+                icon: const Icon(
+                  Icons.today,
+                  color: Colors.black,
+                )),
+          ),
+          ListTile(
+            title: const Text("Contacts"),
+            leading: IconButton(
+                onPressed: () {
+                  print("Pressed contacts");
+                },
+                icon: const Icon(
+                  Icons.contacts,
+                  color: Colors.black,
+                )),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Text(
+              "OTHER",
+              style: TextStyle(
+                  fontWeight: FontWeight.w600,
+                  color: Colors.grey.shade700,
+                  letterSpacing: 1),
+            ),
+          ),
+          ListTile(
+            title: const Text("Settings"),
+            leading: IconButton(
+              onPressed: () {
+                print("Pressed Settings");
+              },
+              icon: const Icon(
+                Icons.settings,
+                color: Colors.black,
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
