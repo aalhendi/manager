691aa3cbc58fe45bcae750e3d6c8e84154fc051d
aalhendi <abdulrazaq.alhendi@gmail.com>
2022-01-05 12:44:33 +0300

    Implemented theme colorschemes

--- app/lib/controller/theme_notifier.dart
+++ app/lib/controller/theme_notifier.dart
@@ -2,15 +2,37 @@ import 'package:flutter/material.dart';
 
 class ThemeNotifier extends ChangeNotifier {
   static final darkTheme = ThemeData(
-      primaryColor: Colors.black,
-      scaffoldBackgroundColor: Colors.grey.shade900,
-      colorScheme: const ColorScheme.dark());
+    colorScheme: ColorScheme(
+        primary: Colors.grey.shade900,
+        primaryVariant: Colors.black,
+        secondary: Colors.blue.shade300,
+        secondaryVariant: Colors.grey.shade200,
+        surface: Colors.grey.shade600,
+        background: Colors.grey.shade900,
+        error: Colors.red,
+        onPrimary: Colors.grey.shade100,
+        onSecondary: Colors.grey.shade900,
+        onSurface: Colors.white,
+        onBackground: Colors.grey.shade100,
+        onError: Colors.yellow,
+        brightness: Brightness.dark),
+  );
 
   static final lightTheme = ThemeData(
-      primaryColor: Colors.white,
-      scaffoldBackgroundColor: Colors.white,
-      primarySwatch: Colors.blue,
-      colorScheme: const ColorScheme.light());
+      colorScheme: ColorScheme(
+          primary: Colors.white,
+          primaryVariant: Colors.grey.shade100,
+          secondary: Colors.blue,
+          secondaryVariant: Colors.grey.shade400,
+          surface: Colors.grey.shade50,
+          background: Colors.grey.shade50,
+          error: Colors.red,
+          onPrimary: Colors.grey.shade900,
+          onSecondary: Colors.grey.shade900,
+          onSurface: Colors.black,
+          onBackground: Colors.black,
+          onError: Colors.yellow,
+          brightness: Brightness.light));
 
   ThemeMode themeMode = ThemeMode.light;
   ThemeData themeData = lightTheme;
@@ -18,6 +40,7 @@ class ThemeNotifier extends ChangeNotifier {
 
   void toggleTheme(bool isOn) {
     themeMode = isOn ? ThemeMode.dark : ThemeMode.light;
+    themeData = isOn ? darkTheme : lightTheme;
     notifyListeners();
   }
 }
